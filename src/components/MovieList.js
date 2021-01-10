150bd17e599dc20210f2d125f33e86406d6a55c8
David Kamura <davidkamura@gmail.com>
2021-01-10 18:05:34 +0300

    Fixed omdb httpRequest

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,33 +2,37 @@ import React, { useState, useEffect } from 'react';
 import './Components.css';
 
 function MovieList(props) {
-    const [movieData, setData] = useState([]);
+    const [movieData, setMovieData] = useState(null);
 
+    
     useEffect(() => {
         let url = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${props.searchValue.replace(/-+/g, " ")}`;
         let xml = new XMLHttpRequest();
-        xml.onload = function() {
-            setData(xml.response);
-        }
         xml.open("GET", url, true);
+        xml.onload = () => {
+            fetchData(xml.response);
+        }
         xml.send();
     })
-    
-    let data = JSON.parse(movieData);
-    let movies = data.Search.map((movie) => {
-        return (
-            <div key={movie.imdbID} className="card">
-                <img src={movie.Poster} alt={`Movie: ${movie.Title}`} width="100" className="img-responsive card-img" />
-                <div className="card-body">
-                    <div className="card-title">
-                        <span>{movie.Title}</span>
+
+    function fetchData(data) {
+        setMovieData(data);
+    }
+
+    let movie = JSON.parse(movieData);
+    let movies = movieData !== null ? movie.Search.map((movie) => {
+            return (
+                <div key={movie.imdbID} className="card">
+                    <img src={movie.Poster} alt={`Movie: ${movie.Title}`} width="100" className="img-responsive card-img" />
+                    <div className="card-body">
+                        <div className="card-title">
+                            <span>{movie.Title}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
-    });
+            );
+    }) : <div></div>;
 
-    
     return (
         <div className="container-fluid" style={{paddingTop: "20px", minHeight: "91.5vh"}}>
             <div className="row px-0 d-flex align-items-center justify-content-center">
@@ -37,7 +41,7 @@ function MovieList(props) {
                 </div>
                 <div className="col-12 d-flex justify-content-center">
                     <div className="movie-list-container">
-                        {movies}
+                        {movieData !== null ? movies : <div></div>}
                     </div>
                 </div>
             </div>
